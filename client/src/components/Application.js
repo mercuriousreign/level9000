df9d5ca737489ccd939b4f16b4b0c24897c97d0b
YoussefBenAmmar <benammar.y@gmail.com>
2023-03-17 20:36:29 -0400

    initial navbar

---
+++ client/src/components/Application.js
@@ -0,0 +1,14 @@
+
+import "./Application.css";
+import Navbar from './navbar';
+
+
+export default function Application(props) {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>let's go</h1> 
+    </div>
+  );
+}
+
