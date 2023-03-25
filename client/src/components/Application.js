842b669328c5c151ec5e9a35702b5395deb5bf4d
DanielYuanxk <dyxk1234@hotmail.com>
2023-03-25 14:48:30 -0400

    ready to merge to main

--- client/src/components/Application.js
+++ client/src/components/Application.js
@@ -106,7 +106,7 @@ export default function Application(props) {
                   />
                 </div>
               ) : (
-                <Login />
+                <div>Loading</div>
               )
             }
           />
